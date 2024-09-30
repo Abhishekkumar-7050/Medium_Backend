ca677e111077b051ff6c1538fdefe84d8c3188d1
Abhishekkumar-7050 <abhishekofficial7050>
2024-10-01 03:19:38 +0530

    ready user signup jwt token

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,53 @@
 import { Hono } from 'hono';
+import { PrismaClient } from '@prisma/client/edge'
+import { withAccelerate } from '@prisma/extension-accelerate'
+import { decode, sign, verify } from 'hono/jwt'
+
 
 // Create the main Hono app
-const app = new Hono();
+const app = new Hono<{   // generic code  it means typecasting that wheneve dadabse_url used its basiclly means
+    //  its alwais be string 
+	Bindings: {
+		DATABASE_URL: string,
+        JWT_SECRET:string
+	}
+}>();
 
-app.post('/api/v1/signup', (c) => {
+app.post('/api/v1/signin', (c) => {
 	return c.text('signup route')
 })
 
 
 app.get('/',(c)=>{
-     return c.text("tested ok ");
-})
 
-app.post('/api/v1/signin', (c) => {
-	return c.text('signin route')
+
+     return c.text("tested ok ");
+});
+//@ts-ignore
+app.post('/api/v1/signup',async(c) => {
+    const prisma = new PrismaClient({
+        datasourceUrl: c.env.DATABASE_URL,
+    }).$extends(withAccelerate())
+
+    const  body = await  c.req.json();
+     const user =  await  prisma.user.create({
+        data:{
+            email : body.email,
+            password : body.password
+        },
+     })
+      if(!user){
+        
+         c.status(501)
+         return c.json({ 
+            message: "failed to register "
+         });
+      }
+
+const token = await sign({userId: user.id}, c.env.JWT_SECRET);
+
+
+	return c.json({jwt:token});
 })
 
 app.get('/api/v1/blog/:id', (c) => {
