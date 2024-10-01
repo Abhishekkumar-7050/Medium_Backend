28ab665ac5db8bb853f02a4fbafb3096461cf408
Abhishekkumar-7050 <abhishekofficial7050>
2024-10-02 01:36:10 +0530

    all routes and and api end point for user and blog is ready

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { Hono } from 'hono';
-import { PrismaClient } from '@prisma/client/edge'
-import { withAccelerate } from '@prisma/extension-accelerate'
-import { decode, sign, verify } from 'hono/jwt'
+
+import { userRouter } from './routes/user';
+import { blogRouter } from './routes/blog';
 
 
 // Create the main Hono app
@@ -13,56 +13,18 @@ const app = new Hono<{   // generic code  it means typecasting that wheneve dada
 	}
 }>();
 
-app.post('/api/v1/signin', (c) => {
-	return c.text('signup route')
+app.get('/', (c) => {
+	return c.text('tested ok')
 })
 
 
-app.get('/',(c)=>{
 
-
-     return c.text("tested ok ");
-});
 //@ts-ignore
-app.post('/api/v1/signup',async(c) => {
-    const prisma = new PrismaClient({
-        datasourceUrl: c.env.DATABASE_URL,
-    }).$extends(withAccelerate())
-
-    const  body = await  c.req.json();
-     const user =  await  prisma.user.create({
-        data:{
-            email : body.email,
-            password : body.password
-        },
-     })
-      if(!user){
-        
-         c.status(501)
-         return c.json({ 
-            message: "failed to register "
-         });
-      }
-
-const token = await sign({userId: user.id}, c.env.JWT_SECRET);
 
 
-	return c.json({jwt:token});
-})
 
-app.get('/api/v1/blog/:id', (c) => {
-	const id = c.req.param('id')
-	console.log(id);
-	return c.text('get blog route')
-})
+app.route('/api/v1/user', userRouter)
+app.route('/api/v1/book', blogRouter)
 
-app.post('/api/v1/blog', (c) => {
-
-	return c.text('signin route')
-})
-
-app.put('/api/v1/blog', (c) => {
-	return c.text('signin route')
-})
 
 export default app;
